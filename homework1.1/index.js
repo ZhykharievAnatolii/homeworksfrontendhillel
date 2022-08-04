326f54da929795de2542d2f7d671a21c3e229656
ZhykharievAnatolii <nekijt87@gmail.com>
2022-08-04 13:06:30 +0300

    do second homework

---
+++ homework1.1/index.js
@@ -0,0 +1,4 @@
+const firstnumber= Number(prompt('Please type your number here'));
+const secondnumber= + prompt('Please type your number here');
+const result= firstnumber + secondnumber;
+alert(result);
